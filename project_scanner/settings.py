a53efd164b12d1155b6ca12eae50482a41eea75f
apollojain@gmail.com <apollojain@gmail.com>
2016-08-14 01:40:57 -0700

    static root set

--- project_scanner/settings.py
+++ project_scanner/settings.py
@@ -101,6 +101,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
