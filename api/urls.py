2052600f3aa9bf83e247caced22b66f12d11d3a0
Sam Holladay <sam.holladay@berkeley.edu>
2016-09-22 00:27:14 -0700

    Making default url

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from django.conf.urls import include
+from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
-    url(r'^scans/$', views.ScanList.as_view()),
-    url(r'^scans/(?P<pk>[0-9]+)/$', views.ScanDetail.as_view()),
-    url(r'^scanners/$', views.ScannerList.as_view()),
-    url(r'^scanners/(?P<pk>[0-9]+)/$', views.ScannerDetail.as_view()),
-    url(r'^locations/$', views.LocationList.as_view()),
-    url(r'^locations/(?P<pk>[0-9]+)/$', views.LocationDetail.as_view()),
+    url(r'^$', RedirectView.as_view(pattern_name='scannerlist')),
+    url(r'^scans/$', views.ScanList.as_view(),name='scanlist'),
+    url(r'^scans/(?P<pk>[0-9]+)/$', views.ScanDetail.as_view(),name='scandetail'),
+    url(r'^scanners/$', views.ScannerList.as_view(), name='scannerlist'),
+    url(r'^scanners/(?P<pk>[0-9]+)/$', views.ScannerDetail.as_view(),name='scannerdetail'),
+    url(r'^locations/$', views.LocationList.as_view(),name='locationlist'),
+    url(r'^locations/(?P<pk>[0-9]+)/$', views.LocationDetail.as_view(),name='locationdetail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
